161e0e2abe6cc8776141d6b6822075cbaee04437
Álvaro Lemos <alvarolemos@gmail.com>
2015-11-02 18:22:46 -0200

    Cria representacao da lista dos niveis atraves de uma lista, com um objeto para cada nivel

--- js/app.js
+++ js/app.js
@@ -1,3 +1,64 @@
 (function() {
+
+  // Modulo principal do jogo
   var app = angular.module('jogo', []);
+
+  // Lista dos niveis do jogo
+  var listaNiveis = [
+    {
+      'nivel': '01',
+      'titulo': '',
+      'isDisponivel': true,
+      'desafiosCompletados': 1,
+    },
+    {
+      'nivel': '02',
+      'titulo': '',
+      'isDisponivel': false,
+      'desafiosCompletados': 0,
+    },
+    {
+      'nivel': '03',
+      'titulo': '',
+      'isDisponivel': false,
+      'desafiosCompletados': 0,
+    },
+    {
+      'nivel': '04',
+      'titulo': '',
+      'isDisponivel': false,
+      'desafiosCompletados': 0,
+    },
+    {
+      'nivel': '05',
+      'titulo': '',
+      'isDisponivel': false,
+      'desafiosCompletados': 0,
+    },
+    {
+      'nivel': '06',
+      'titulo': '',
+      'isDisponivel': false,
+      'desafiosCompletados': 0,
+    },
+    {
+      'nivel': '07',
+      'titulo': '',
+      'isDisponivel': false,
+      'desafiosCompletados': 0,
+    },
+    {
+      'nivel': '08',
+      'titulo': '',
+      'isDisponivel': false,
+      'desafiosCompletados': 0,
+    },
+    {
+      'nivel': '09',
+      'titulo': '',
+      'isDisponivel': false,
+      'desafiosCompletados': 0,
+    },
+  ];
+
 })();
