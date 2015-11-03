820dece3859f4cf46b8900e4d06fc2cde21ad7b4
Álvaro Lemos <alvarolemos@gmail.com>
2015-11-02 23:49:45 -0200

    Conserta sintaxe da lista de niveis

--- js/app.js
+++ js/app.js
@@ -14,56 +14,58 @@
       'nivel': '01',
       'titulo': '',
       'isDisponivel': true,
-      'desafiosCompletados': 1,
+      'desafiosCompletados': 1
     },
     {
       'nivel': '02',
       'titulo': '',
       'isDisponivel': false,
-      'desafiosCompletados': 0,
+      'desafiosCompletados': 0
     },
     {
       'nivel': '03',
       'titulo': '',
       'isDisponivel': false,
-      'desafiosCompletados': 0,
+      'desafiosCompletados': 0
     },
     {
       'nivel': '04',
       'titulo': '',
       'isDisponivel': false,
-      'desafiosCompletados': 0,
+      'desafiosCompletados': 0
     },
     {
       'nivel': '05',
       'titulo': '',
       'isDisponivel': false,
-      'desafiosCompletados': 0,
+      'desafiosCompletados': 0
     },
     {
       'nivel': '06',
       'titulo': '',
       'isDisponivel': false,
-      'desafiosCompletados': 0,
+      'desafiosCompletados': 0
     },
     {
       'nivel': '07',
       'titulo': '',
       'isDisponivel': false,
-      'desafiosCompletados': 0,
+      'desafiosCompletados': 0
     },
     {
       'nivel': '08',
       'titulo': '',
       'isDisponivel': false,
-      'desafiosCompletados': 0,
+      'desafiosCompletados': 0
     },
     {
       'nivel': '09',
       'titulo': '',
       'isDisponivel': false,
-      'desafiosCompletados': 0,
+      'desafiosCompletados': 0
     }
   ];
+
+
 
 })();
