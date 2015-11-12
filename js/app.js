44f753645406427ff68ec0ede98367b1ad98120d
Álvaro Lemos <alvarolemos@gmail.com>
2015-11-12 20:23:17 -0200

    Apaga lista de niveis do arquivo app.js, pois agora ela eh puxada externamente

--- js/app.js
+++ js/app.js
@@ -61,64 +61,6 @@
     };
   });
 
-  // Lista dos niveis do jogo
-  var listaNiveis = [
-    {
-      'nivel': '01',
-      'titulo': '',
-      'isDisponivel': true,
-      'desafiosCompletados': 1
-    },
-    {
-      'nivel': '02',
-      'titulo': '',
-      'isDisponivel': false,
-      'desafiosCompletados': 0
-    },
-    {
-      'nivel': '03',
-      'titulo': '',
-      'isDisponivel': false,
-      'desafiosCompletados': 0
-    },
-    {
-      'nivel': '04',
-      'titulo': '',
-      'isDisponivel': false,
-      'desafiosCompletados': 0
-    },
-    {
-      'nivel': '05',
-      'titulo': '',
-      'isDisponivel': false,
-      'desafiosCompletados': 0
-    },
-    {
-      'nivel': '06',
-      'titulo': '',
-      'isDisponivel': false,
-      'desafiosCompletados': 0
-    },
-    {
-      'nivel': '07',
-      'titulo': '',
-      'isDisponivel': false,
-      'desafiosCompletados': 0
-    },
-    {
-      'nivel': '08',
-      'titulo': '',
-      'isDisponivel': false,
-      'desafiosCompletados': 0
-    },
-    {
-      'nivel': '09',
-      'titulo': '',
-      'isDisponivel': false,
-      'desafiosCompletados': 0
-    }
-  ];
-
   // Lista de desafios
   var listaDesafios = [
     [
