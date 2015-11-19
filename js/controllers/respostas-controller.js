68847c72294bddecb4018e578988f9d790519566
Álvaro Lemos <alvarolemos@gmail.com>
2015-11-19 10:51:40 -0200

    Refatora pagina de desafio

--- js/controllers/respostas-controller.js
+++ js/controllers/respostas-controller.js
@@ -4,11 +4,20 @@ angular.module('jogo')
 });
 
 var listasRespostas = [
-  { 'resposta': 'Corridas', 'pontos': '40', 'isRespondida': false },
-  { 'resposta': 'Mestre do Scrum', 'pontos': '15', 'isRespondida': false },
-  { 'resposta': 'Dono do Produto', 'pontos': '10', 'isRespondida': false },
-  { 'resposta': 'Time', 'pontos': '11', 'isRespondida': false },
-  { 'resposta': 'Fluxo de Processo', 'pontos': '04', 'isRespondida': false },
-  { 'resposta': 'Reuniões Diárias', 'pontos': '09', 'isRespondida': false },
-  { 'resposta': 'Revisão', 'pontos': '05', 'isRespondida': false }
+  { 'resposta': '', 'pontos': '40', 'numero': 1 },
+  { 'resposta': '', 'pontos': '15', 'numero': 2 },
+  { 'resposta': '', 'pontos': '10', 'numero': 3 },
+  { 'resposta': '', 'pontos': '11', 'numero': 4 },
+  { 'resposta': '', 'pontos': '04', 'numero': 5 },
+  { 'resposta': '', 'pontos': '09', 'numero': 6 },
+  { 'resposta': '', 'pontos': '05', 'numero': 7 }
 ];
+// var listasRespostas = [
+//   { 'resposta': 'Corridas', 'pontos': '40', 'isRespondida': false },
+//   { 'resposta': 'Mestre do Scrum', 'pontos': '15', 'isRespondida': true },
+//   { 'resposta': 'Dono do Produto', 'pontos': '10', 'isRespondida': false },
+//   { 'resposta': 'Time', 'pontos': '11', 'isRespondida': false },
+//   { 'resposta': 'Fluxo de Processo', 'pontos': '04', 'isRespondida': false },
+//   { 'resposta': 'Reuniões Diárias', 'pontos': '09', 'isRespondida': false },
+//   { 'resposta': 'Revisão', 'pontos': '05', 'isRespondida': false }
+// ];
