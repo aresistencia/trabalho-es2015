5e732d496992cbdb98f8712e55434cd0918442ff
Álvaro Lemos <alvarolemos@gmail.com>
2015-11-23 02:20:46 -0300

    Refatora template lista-niveis

--- js/controllers/lista-niveis-controller.js
+++ js/controllers/lista-niveis-controller.js
@@ -1,8 +1,7 @@
 angular.module('jogo')
-.controller('ListaNiveisController', [ '$http', function($http) {
-  var controller = this;
-  controller.niveis = [];
+.controller('ListaNiveisController', [ '$scope', '$http', function($scope, $http) {
+  $scope.niveis = [];
   $http.get('https://api.myjson.com/bins/15fz9').success(function(data) {
-    controller.niveis = data;
+    $scope.niveis = data;
   });
 }]);
