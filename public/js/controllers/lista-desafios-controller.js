4ee4b743d3f198b8347f33d8f030b763c4e7b10d
Álvaro Lemos <alvarolemos@gmail.com>
2015-11-23 19:08:31 -0300

    Refatora carregamento de dados, antes puxados via arquivos JSON por myjson.com, por objetos locais

--- public/js/controllers/lista-desafios-controller.js
+++ public/js/controllers/lista-desafios-controller.js
@@ -1,7 +1,27 @@
 angular.module('jogo')
 .controller('ListaDesafiosController', [ '$scope', '$http', function($scope, $http) {
-  $scope.desafios = [];
-  $http.get('https://api.myjson.com/bins/115np').success(function(data) {
-    $scope.desafios = data;
-  });
+  // $scope.desafios = [];
+  // $http.get('https://api.myjson.com/bins/115np').success(function(data) {
+  //   $scope.desafios = data;
+  // });
+  $scope.desafios = [
+    {
+      "numero": 1,
+      "titulo": "Animais que nascem de um ovo",
+      "pontuacao": 0,
+      "isCompletado": false
+    },
+    {
+      "numero": 2,
+      "titulo": "Frutas com sementes ou caroço",
+      "pontuacao": 0,
+      "isCompletado": false
+    },
+    {
+      "numero": 3,
+      "titulo": "Alguma outra coisa",
+      "pontuacao": 0,
+      "isCompletado": false
+    }
+  ];
 }]);
--- public/js/controllers/lista-niveis-controller.js
+++ public/js/controllers/lista-niveis-controller.js
@@ -1,7 +1,63 @@
 angular.module('jogo')
 .controller('ListaNiveisController', [ '$scope', '$http', function($scope, $http) {
-  $scope.niveis = [];
-  $http.get('https://api.myjson.com/bins/15fz9').success(function(data) {
-    $scope.niveis = data;
-  });
+  // $scope.niveis = [];
+  // $http.get('https://api.myjson.com/bins/15fz9').success(function(data) {
+  //   $scope.niveis = data;
+  // });
+  $scope.niveis = [
+      {
+        "numero": 1,
+        "titulo": "",
+        "isDisponivel": true,
+        "desafiosCompletados": 1
+      },
+      {
+        "numero": 2,
+        "titulo": "",
+        "isDisponivel": false,
+        "desafiosCompletados": 0
+      },
+      {
+        "numero": 3,
+        "titulo": "",
+        "isDisponivel": false,
+        "desafiosCompletados": 0
+      },
+      {
+        "numero": 4,
+        "titulo": "",
+        "isDisponivel": false,
+        "desafiosCompletados": 0
+      },
+      {
+        "numero": 5,
+        "titulo": "",
+        "isDisponivel": false,
+        "desafiosCompletados": 0
+      },
+      {
+        "numero": 6,
+        "titulo": "",
+        "isDisponivel": false,
+        "desafiosCompletados": 0
+      },
+      {
+        "numero": 7,
+        "titulo": "",
+        "isDisponivel": false,
+        "desafiosCompletados": 0
+      },
+      {
+        "numero": 8,
+        "titulo": "",
+        "isDisponivel": false,
+        "desafiosCompletados": 0
+      },
+      {
+        "numero": 9,
+        "titulo": "",
+        "isDisponivel": false,
+        "desafiosCompletados": 0
+      }
+    ];
 }]);
