600a209468afd4f840417316f016d3e1bd21ac82
Álvaro Lemos <alvarolemos@gmail.com>
2015-11-23 20:36:11 -0300

    Cria module para o aplicativo do servidor e coloca ele para rodar em uma porta via o arquivo server.js

---
+++ app.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/lista-niveis', function(request, response) {
+  var listaNiveis = [
+    {
+      "numero": 1,
+      "titulo": "",
+      "isDisponivel": true,
+      "desafiosCompletados": 1
+    },
+    {
+      "numero": 2,
+      "titulo": "",
+      "isDisponivel": false,
+      "desafiosCompletados": 0
+    },
+    {
+      "numero": 3,
+      "titulo": "",
+      "isDisponivel": false,
+      "desafiosCompletados": 0
+    },
+    {
+      "numero": 4,
+      "titulo": "",
+      "isDisponivel": false,
+      "desafiosCompletados": 0
+    },
+    {
+      "numero": 5,
+      "titulo": "",
+      "isDisponivel": false,
+      "desafiosCompletados": 0
+    },
+    {
+      "numero": 6,
+      "titulo": "",
+      "isDisponivel": false,
+      "desafiosCompletados": 0
+    },
+    {
+      "numero": 7,
+      "titulo": "",
+      "isDisponivel": false,
+      "desafiosCompletados": 0
+    },
+    {
+      "numero": 8,
+      "titulo": "",
+      "isDisponivel": false,
+      "desafiosCompletados": 0
+    },
+    {
+      "numero": 9,
+      "titulo": "",
+      "isDisponivel": false,
+      "desafiosCompletados": 0
+    }
+  ];
+  response.json(listaNiveis);
+
+});
+
+module.exports = app;
