ff3812d00f34baa92210aabebfb7bf68a3ce0244
Álvaro Lemos <alvarolemos@gmail.com>
2015-11-23 19:04:16 -0300

    Refatora server.js, trocando sendFile pelo middleware express.static

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(request, response) {
-  response.sendfile(__dirname + '/public/index.html');
-});
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
-app.listen(3000);
+app.listen(3000, function() {
+  console.log("Rodando aplicacao na porta 3000...");
+});
