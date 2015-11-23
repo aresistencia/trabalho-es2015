3e11f0e2babc3fe669d97f4ab708edc80e515f63
Álvaro Lemos <alvarolemos@gmail.com>
2015-11-23 19:20:28 -0300

    Puxa lista de niveis pelo server.js

--- server.js
+++ server.js
@@ -3,6 +3,67 @@ var app = express();
 
 app.use(express.static('public'));
 
+app.get('/lista-niveis', function(request, response) {
+  var listaNiveis = [
+    {
+      "numero": 1,
+      "titulo": "",
+      "isDisponivel": true,
+      "desafiosCompletados": 1
+    },
+    {
+      "numero": 2,
+      "titulo": "",
+      "isDisponivel": false,
+      "desafiosCompletados": 0
+    },
+    {
+      "numero": 3,
+      "titulo": "",
+      "isDisponivel": false,
+      "desafiosCompletados": 0
+    },
+    {
+      "numero": 4,
+      "titulo": "",
+      "isDisponivel": false,
+      "desafiosCompletados": 0
+    },
+    {
+      "numero": 5,
+      "titulo": "",
+      "isDisponivel": false,
+      "desafiosCompletados": 0
+    },
+    {
+      "numero": 6,
+      "titulo": "",
+      "isDisponivel": false,
+      "desafiosCompletados": 0
+    },
+    {
+      "numero": 7,
+      "titulo": "",
+      "isDisponivel": false,
+      "desafiosCompletados": 0
+    },
+    {
+      "numero": 8,
+      "titulo": "",
+      "isDisponivel": false,
+      "desafiosCompletados": 0
+    },
+    {
+      "numero": 9,
+      "titulo": "",
+      "isDisponivel": false,
+      "desafiosCompletados": 0
+    }
+  ];
+  response.json(listaNiveis);
+
+});
+
 app.listen(3000, function() {
   console.log("Rodando aplicacao na porta 3000...");
 });
