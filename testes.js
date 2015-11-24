fa4b135153c2397771450228feaa0825c511950e
Álvaro Lemos <alvarolemos@gmail.com>
2015-11-24 08:36:10 -0200

    Passa em teste que checa se a rota raiz retorna conteuto HTML

--- testes.js
+++ testes.js
@@ -2,9 +2,17 @@ var request = require('supertest'),
     app = require('./app');
 
 describe('Faz requisicao para rota raiz', function() {
+
   it('Retorna codigo de status 200', function(done) {
     request(app)
       .get('/')
       .expect(200, done);
   });
+
+  it('Retorna um formato HTML', function(done) {
+    request(app)
+      .get('/')
+      .expect('Content-Type', /html/, done);
+  });
+
 });
