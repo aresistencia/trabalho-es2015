ad47612b6bb2bfb69a9714e74ee6ed61d65df6d4
Álvaro Lemos <alvarolemos@gmail.com>
2015-11-24 08:26:19 -0200

    Rota raiz retorna codigo de status 200

--- testes.js
+++ testes.js
@@ -1,2 +1,10 @@
 var request = require('supertest'),
     app = require('./app');
+
+describe('Faz requisicao para rota raiz', function() {
+  it('Retorna codigo de status 200', function(done) {
+    request(app)
+      .get('/')
+      .expect(200, done);
+  });
+});
