7c0b99c6b46261eb0bbf754b62ec5ea5b1aa5046
Álvaro Lemos <alvarolemos@gmail.com>
2015-11-24 08:45:02 -0200

    Passa em testes que checa se a rota /lista-niveis esta passando a lista de niveis

--- testes.js
+++ testes.js
@@ -16,3 +16,82 @@ describe('Faz requisicao para rota raiz', function() {
   });
 
 });
+
+describe('Faz requisicao para a lista de niveis', function() {
+
+  it('Retorna codigo de status 200', function(done) {
+    request(app)
+      .get('/lista-niveis')
+      .expect(200, done);
+  });
+
+  it('Retorna um arquivo JSON', function(done) {
+    request(app)
+      .get('/lista-niveis')
+      .expect('Content-Type', /json/, done);
+  });
+
+  it('Retorna lista de niveis', function(done) {
+
+    var listaNiveis = [
+      {
+        "numero": 1,
+        "titulo": "",
+        "isDisponivel": true,
+        "desafiosCompletados": 1
+      },
+      {
+        "numero": 2,
+        "titulo": "",
+        "isDisponivel": false,
+        "desafiosCompletados": 0
+      },
+      {
+        "numero": 3,
+        "titulo": "",
+        "isDisponivel": false,
+        "desafiosCompletados": 0
+      },
+      {
+        "numero": 4,
+        "titulo": "",
+        "isDisponivel": false,
+        "desafiosCompletados": 0
+      },
+      {
+        "numero": 5,
+        "titulo": "",
+        "isDisponivel": false,
+        "desafiosCompletados": 0
+      },
+      {
+        "numero": 6,
+        "titulo": "",
+        "isDisponivel": false,
+        "desafiosCompletados": 0
+      },
+      {
+        "numero": 7,
+        "titulo": "",
+        "isDisponivel": false,
+        "desafiosCompletados": 0
+      },
+      {
+        "numero": 8,
+        "titulo": "",
+        "isDisponivel": false,
+        "desafiosCompletados": 0
+      },
+      {
+        "numero": 9,
+        "titulo": "",
+        "isDisponivel": false,
+        "desafiosCompletados": 0
+      }
+    ];
+
+    request(app)
+      .get('/lista-niveis')
+      .expect(listaNiveis, done);
+  });
+});
