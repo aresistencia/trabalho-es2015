f00bd8ef11eede4dbadb8c9786b39bb98791efb1
Álvaro Lemos <alvarolemos@gmail.com>
2015-11-24 13:57:39 -0200

    Passe em teste que checa se a pontuacao das resposta do desafio 1 do nivel 1 estao chegando

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ app.get('/lista-niveis', function(request, response) {
 });
 
 app.get('/nivel/1', function(request, response) {
-  response.send(infoDesafios[1]);
+  response.json(infoDesafios[1]);
+});
+
+app.get('/nivel/1/desafio/1', function(request, response) {
+  response.json(infoRespostas[1].desafios[1]);
 });
 
 // Variaveis para auxiliar as respostas das requisicoes
@@ -83,6 +87,30 @@ var infoDesafios = [
       { 'titulo': 'Scrum', 'pontuacao': 0, 'id': 1 },
       { 'titulo': 'XP', 'pontuacao': 0, 'id': 2 },
       { 'titulo': 'Kanban', 'pontuacao': 0, 'id': 3 }
+    ]
+  }
+];
+
+// Lista com a pontuacao da resposta dos desafios de cada nivel
+var infoRespostas = [
+  { /* Objeto vazio para facilitar a indexacao: nivel 1 -> indice 1 */ },
+  {
+    "nivelID": 1,
+    "desafios": [
+      { /* Objeto vazio para facilitar a indexacao: desafio 1 -> desafio 1 */ },
+      {
+        "id": 1,
+        "titulo": "Scrum",
+        "respostas": [
+          { "id": 1, "pontos": 40, "resposta": "" },
+          { "id": 2, "pontos": 15, "resposta": "" },
+          { "id": 3, "pontos": 10, "resposta": "" },
+          { "id": 4, "pontos": 11, "resposta": "" },
+          { "id": 5, "pontos": 4, "resposta": "" },
+          { "id": 6, "pontos": 9, "resposta": "" },
+          { "id": 7, "pontos": 5, "resposta": "" }
+        ]
+      }
     ]
   }
 ];
--- testes.js
+++ testes.js
@@ -41,7 +41,7 @@ describe('Faz requisicao para a lista de niveis', function() {
 
 });
 
-// Testes para as rotas '/nivel/:nivel'
+// Testes para a rota '/nivel/1'
 describe('Faz requisicao para a lista de desafios do nivel 1', function() {
 
   it('Retorna codigo de status 200', function(done) {
@@ -60,6 +60,29 @@ describe('Faz requisicao para a lista de desafios do nivel 1', function() {
     request(app)
       .get('/nivel/1')
       .expect(infoDesafios[1], done);
+  });
+
+});
+
+// Teste para a rota '/nivel/1/desafio/1'
+describe('Faz requisicao para a lista de pontuacao das respostas do desafio 1 do nivel 1', function() {
+
+  it('Retorna codigo de status 200', function(done) {
+    request(app)
+      .get('/nivel/1/desafio/1')
+      .expect(200, done);
+  });
+
+  it('Retorna um arquivo no formato JSON', function(done) {
+    request(app)
+      .get('/nivel/1/desafio/1')
+      .expect('Content-Type', /json/, done);
+  });
+
+  it('Retorna uma lista com a pontuacao das respostas do desafio 1 do nivel 1', function(done) {
+    request(app)
+      .get('/nivel/1/desafio/1')
+      .expect(infoRespostas[1].desafios[1], done);
   });
 
 });
@@ -126,14 +149,38 @@ var listaNiveis = [
 var infoDesafios = [
   { /* Objeto vazio para facilitar a indexacao: nivel 1 -> indice 1 */ },
   {
-    'nivel': {
-      'titulo': 'Metodologias Ágeis',
-      'id': 1
+    "nivel": {
+      "titulo": "Metodologias Ágeis",
+      "id": 1
     },
-    'desafios': [
-      { 'titulo': 'Scrum', 'pontuacao': 0, 'id': 1 },
-      { 'titulo': 'XP', 'pontuacao': 0, 'id': 2 },
-      { 'titulo': 'Kanban', 'pontuacao': 0, 'id': 3 }
+    "desafios": [
+      { "titulo": "Scrum", "pontuacao": 0, "id": 1 },
+      { "titulo": "XP", "pontuacao": 0, "id": 2 },
+      { "titulo": "Kanban", "pontuacao": 0, "id": 3 }
+    ]
+  }
+];
+
+// Lista com a pontuacao da resposta dos desafios de cada nivel
+var infoRespostas = [
+  { /* Objeto vazio para facilitar a indexacao: nivel 1 -> indice 1 */ },
+  {
+    "nivelID": 1,
+    "desafios": [
+      { /* Objeto vazio para facilitar a indexacao: desafio 1 -> desafio 1 */ },
+      {
+        "id": 1,
+        "titulo": "Scrum",
+        "respostas": [
+          { "id": 1, "pontos": 40, "resposta": "" },
+          { "id": 2, "pontos": 15, "resposta": "" },
+          { "id": 3, "pontos": 10, "resposta": "" },
+          { "id": 4, "pontos": 11, "resposta": "" },
+          { "id": 5, "pontos": 4, "resposta": "" },
+          { "id": 6, "pontos": 9, "resposta": "" },
+          { "id": 7, "pontos": 5, "resposta": "" }
+        ]
+      }
     ]
   }
 ];
