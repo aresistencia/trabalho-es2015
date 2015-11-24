c3f3375b4cf54d0c1db0db904dc414fd87f18af4
Álvaro Lemos <alvarolemos@gmail.com>
2015-11-24 09:01:26 -0200

    Refatora escopo das variaveis da aplicacao do servidor

--- app.js
+++ app.js
@@ -4,64 +4,68 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('/lista-niveis', function(request, response) {
-  var listaNiveis = [
-    {
-      "numero": 1,
-      "titulo": "",
-      "isDisponivel": true,
-      "desafiosCompletados": 1
-    },
-    {
-      "numero": 2,
-      "titulo": "",
-      "isDisponivel": false,
-      "desafiosCompletados": 0
-    },
-    {
-      "numero": 3,
-      "titulo": "",
-      "isDisponivel": false,
-      "desafiosCompletados": 0
-    },
-    {
-      "numero": 4,
-      "titulo": "",
-      "isDisponivel": false,
-      "desafiosCompletados": 0
-    },
-    {
-      "numero": 5,
-      "titulo": "",
-      "isDisponivel": false,
-      "desafiosCompletados": 0
-    },
-    {
-      "numero": 6,
-      "titulo": "",
-      "isDisponivel": false,
-      "desafiosCompletados": 0
-    },
-    {
-      "numero": 7,
-      "titulo": "",
-      "isDisponivel": false,
-      "desafiosCompletados": 0
-    },
-    {
-      "numero": 8,
-      "titulo": "",
-      "isDisponivel": false,
-      "desafiosCompletados": 0
-    },
-    {
-      "numero": 9,
-      "titulo": "",
-      "isDisponivel": false,
-      "desafiosCompletados": 0
-    }
-  ];
   response.json(listaNiveis);
-
 });
+
+
+// Variaveis para auxiliar as respostas das requisicoes
+
+// Lista de niveis do jogo
+var listaNiveis = [
+  {
+    "numero": 1,
+    "titulo": "",
+    "isDisponivel": true,
+    "desafiosCompletados": 1
+  },
+  {
+    "numero": 2,
+    "titulo": "",
+    "isDisponivel": false,
+    "desafiosCompletados": 0
+  },
+  {
+    "numero": 3,
+    "titulo": "",
+    "isDisponivel": false,
+    "desafiosCompletados": 0
+  },
+  {
+    "numero": 4,
+    "titulo": "",
+    "isDisponivel": false,
+    "desafiosCompletados": 0
+  },
+  {
+    "numero": 5,
+    "titulo": "",
+    "isDisponivel": false,
+    "desafiosCompletados": 0
+  },
+  {
+    "numero": 6,
+    "titulo": "",
+    "isDisponivel": false,
+    "desafiosCompletados": 0
+  },
+  {
+    "numero": 7,
+    "titulo": "",
+    "isDisponivel": false,
+    "desafiosCompletados": 0
+  },
+  {
+    "numero": 8,
+    "titulo": "",
+    "isDisponivel": false,
+    "desafiosCompletados": 0
+  },
+  {
+    "numero": 9,
+    "titulo": "",
+    "isDisponivel": false,
+    "desafiosCompletados": 0
+  }
+];
 
 module.exports = app;
