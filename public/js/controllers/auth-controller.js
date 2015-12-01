d52409306fbe30d3aa75e11fc73594af0f56e23f
Álvaro Lemos <alvarolemos@gmail.com>
2015-12-01 09:54:25 -0200

    Adiciona controlador para autenticacao

--- public/js/controllers/auth-controller.js
+++ public/js/controllers/auth-controller.js
@@ -1,8 +1,9 @@
 angular.module('jogo')
 .controller('AuthController', ['$scope', '$cookies', function($scope, $cookies) {
-  $scope.usuario = '';
-  $scope.$on('rightAnswer', function(event, card) {
-    $cookies.put('login', card.resposta);
+  $scope.usuario = $cookies.get('login') || '';
+  $scope.$on('loginSuccess', function(event, user) {
+    alert("ALOHA!");
+    $cookies.put('login', user.login);
     $scope.usuario = $cookies.get('login');
   });
 }]);
