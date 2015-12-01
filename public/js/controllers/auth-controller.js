52ae1cebf9390d6abf970194c2691c0b5b8448f9
Álvaro Lemos <alvarolemos@gmail.com>
2015-12-01 14:33:26 -0200

    Faz login na aplicacao cliente via HTTP POST

--- public/js/controllers/auth-controller.js
+++ public/js/controllers/auth-controller.js
@@ -1,9 +1,9 @@
 angular.module('jogo')
 .controller('AuthController', ['$scope', '$cookies', function($scope, $cookies) {
   $scope.usuario = $cookies.get('login') || '';
-  $scope.$on('loginSuccess', function(event, user) {
+  $scope.$on('loginSuccess', function(event, username) {
     alert("ALOHA!");
-    $cookies.put('login', user.login);
+    $cookies.put('login', username);
     $scope.usuario = $cookies.get('login');
   });
 }]);
--- public/js/controllers/login-controller.js
+++ public/js/controllers/login-controller.js
@@ -1,11 +1,24 @@
 angular.module('jogo')
-.controller('LoginController', ['$rootScope', function($rootScope) {
+.controller('LoginController', ['$rootScope', '$http', function($rootScope, $http) {
 
   $rootScope.tryLogin = function(uname, pword) {
-    if (uname === 'admin' && pword === '12345') {
-      var user = { 'login': uname, 'password': pword };
-      $rootScope.$broadcast('loginSuccess', user);
-    }
+    $http({
+      url: '/login',
+      method: 'POST',
+      data: "username=" + uname + "&password=" + pword,
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    }).success(function(data, status, headers, config) {
+      if (data.isSuccessful) {
+        $rootScope.$broadcast('loginSuccess', data.username);  
+      }
+    });
+
+
+
+    // if (uname === 'admin' && pword === '12345') {
+    //   var user = { 'login': uname, 'password': pword };
+    //   $rootScope.$broadcast('loginSuccess', user);
+    // }
   };
 
 }]);
