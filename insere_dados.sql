2c3f832735098dedcf9557a46c2a49b56ef0d9f4
Álvaro Lemos <alvarolemos@gmail.com>
2015-11-30 22:57:22 -0200

    Refatora insercao de dados no banco

--- insere_dados.sql
+++ insere_dados.sql
@@ -1,29 +1,33 @@
-INSERT INTO nivel VALUES (1, 'Métodos Ágeis', 1);
-INSERT INTO nivel VALUES (2, 'Métodos Tradicionais', 0);
-INSERT INTO nivel VALUES (3, '', 0);
-INSERT INTO nivel VALUES (4, '', 0);
-INSERT INTO nivel VALUES (5, '', 0);
-INSERT INTO nivel VALUES (6, '', 0);
-INSERT INTO nivel VALUES (7, '', 0);
-INSERT INTO nivel VALUES (8, '', 0);
-INSERT INTO nivel VALUES (9, '', 0);
+INSERT INTO nivel (id, titulo, is_disponivel)
+VALUES (1, 'Métodos Ágeis', 1),
+       (2, 'Métodos Tradicionais', 0),
+       (3, '', 0),
+       (4, '', 0),
+       (5, '', 0),
+       (6, '', 0),
+       (7, '', 0),
+       (8, '', 0),
+       (9, '', 0);
 
-INSERT INTO desafio VALUES (1, 'Scrum', 0, 1);
-INSERT INTO desafio VALUES (2, 'XP', 0, 1);
-INSERT INTO desafio VALUES (3, 'Kanban', 0, 1);
+INSERT INTO desafio (id, titulo, is_completo, nivel_id)
+VALUES (1, 'Scrum', 0, 1),
+       (2, 'XP', 0, 1),
+       (3, 'Kanban', 0, 1);
 
-INSERT INTO resposta VALUES (1, 40, 'Corridas', 0, 1);
-INSERT INTO resposta VALUES (2, 15, 'Mestre do Scrum', 0, 1);
-INSERT INTO resposta VALUES (3, 10, 'Dono do Produto', 0, 1);
-INSERT INTO resposta VALUES (4, 11, 'Time', 0, 1);
-INSERT INTO resposta VALUES (5, 4, 'Fluxo de Processo', 0, 1);
-INSERT INTO resposta VALUES (6, 9, 'Reuniões Diárias', 0, 1);
-INSERT INTO resposta VALUES (7, 5, 'Revisão', 0, 1);
-
-INSERT INTO resposta VALUES (8, 40, 'Desenvolvimento em Pares', 0, 2);
-INSERT INTO resposta VALUES (9, 15, 'História de Usuário', 0, 2);
-INSERT INTO resposta VALUES (10, 10, 'Keep It Simple', 0, 2);
-INSERT INTO resposta VALUES (11, 11, 'Refatoração', 0, 2);
-INSERT INTO resposta VALUES (12, 4, 'Testes', 0, 2);
-
-INSERT INTO resposta VALUES (13, 40, '', 0, 3);
+INSERT INTO resposta (id, valor, solucao, is_respondida, desafio_id)
+VALUES -- Scrum
+       (1, 40, 'Corridas', 0, 1),
+       (2, 15, 'Mestre do Scrum', 0, 1),
+       (3, 10, 'Dono do Produto', 0, 1),
+       (4, 11, 'Time', 0, 1),
+       (5, 4, 'Fluxo de Processo', 0, 1),
+       (6, 9, 'Reuniões Diárias', 0, 1),
+       (7, 5, 'Revisão', 0, 1),
+       -- XP
+       (8, 40, 'Desenvolvimento em Pares', 0, 2),
+       (9, 15, 'História de Usuário', 0, 2),
+       (10, 10, 'Keep It Simple', 0, 2),
+       (11, 11, 'Refatoração', 0, 2),
+       (12, 4, 'Testes', 0, 2),
+       -- ...
+       (13, 40, '', 0, 3);
