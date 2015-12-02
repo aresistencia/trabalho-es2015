e557de534fea2472a1807e2d23a3c875453c4abd
Álvaro Lemos <alvarolemos@gmail.com>
2015-12-02 15:58:56 -0200

    Redireciona para a pagina de niveis ao logar usuario

--- public/js/controllers/login-controller.js
+++ public/js/controllers/login-controller.js
@@ -1,5 +1,5 @@
 angular.module('jogo')
-.controller('LoginController', ['$rootScope', '$http', function($rootScope, $http) {
+.controller('LoginController', ['$rootScope', '$http', '$location', function($rootScope, $http, $location) {
 
   $rootScope.tryLogin = function(uname, pword) {
     $http({
@@ -10,6 +10,7 @@ angular.module('jogo')
     }).success(function(data, status, headers, config) {
       if (data.isSuccessful) {
         $rootScope.$broadcast('loginSuccess', data);
+        $location.url('/lista-niveis');
       }
     });
   };
