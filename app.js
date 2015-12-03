487531c06d2185576bdb05aeeca8746674427ebb
Álvaro Lemos <alvarolemos@gmail.com>
2015-12-03 00:03:45 -0200

    Registra usuarios no banco pelo site

--- app.js
+++ app.js
@@ -127,8 +127,8 @@ app.post('/login', parseUrlEncoded, function(request, response) {
 app.post('/registro', parseUrlEncoded, function(request, response) {
   var username = request.body.username,
       password = request.body.password,
-      nome = request.body.pnome;// + " " + request.body.snome;
-  db.run("INSERT INTO usuario (username, password, nome) VALUES (" + username + "," + password + "," + nome + ")");
+      nome = request.body.pnome + " " + request.body.snome;
+  db.run("INSERT INTO usuario (username, password, nome) VALUES (?, ?, ?)", username, password, nome);
   response.status(201).json(username);
 });
 
